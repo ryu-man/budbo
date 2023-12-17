de11278cce6ee85dc95d32b0cb7ecdd803ae3b38
abdelhalim <rioman19@gmail.com>
2023-12-16 23:43:48 -0500

    better spacing

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -25,9 +25,9 @@ export function HeroSection() {
 	}, []);
 
 	return (
-		<div className="hero flex flex-col  items-start justify-center relative">
+		<div className="hero flex flex-col  items-start relative">
 
-			<div className="flex flex-col w-fit pt-12 2xl:mr-44 2xl:ml-48">
+			<div className="flex flex-col w-fit pt-16 pl-16 md:pl-6 xl:pl-20 3xl:pl-48">
 				<div className="flex flex-col w-full">
 					<h1 className="mb-12">
 						<span>Move. Maker.</span> <br />
